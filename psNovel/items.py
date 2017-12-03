0d3e72a03df750cb976fdfa687606c61b4f12848
sujun1972 <sujun1972@gmail.com>
2017-12-03 17:50:10 +0100

    first

---
+++ psNovel/items.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Novel(scrapy.Item):
+    # 作品名称
+    title = scrapy.Field()
+    # 作者
+    author = scrapy.Field()
+    # 文案
+    intro = scrapy.Field()
+    # 封面图片
+    poster = scrapy.Field()
+    # 标签
+    tags = scrapy.Field()
+    # 关键字
+    key_leadings = scrapy.Field()
+    key_supportings = scrapy.Field()
+    key_other = scrapy.Field()
+    # 文章类型
+    genre = scrapy.Field()
+    # 作品视角
+    view = scrapy.Field()
+    # 作品风格
+    style = scrapy.Field()
+    # 作品系列
+    series = scrapy.Field()
+    # 文章进度
+    updateStatus = scrapy.Field()
+    # 全文字数
+    wordCount = scrapy.Field()
+    # 是否出版
+    published = scrapy.Field()
+    # 是否已签约
+    signed = scrapy.Field()
+    # 网站简评
+    comment = scrapy.Field()
+
+
+class Chapter(scrapy.Item):
+    novel_id = scrapy.Field()
+    chapter_id = scrapy.Field()
+    chapter_content = scrapy.Field()
+    chapter_summary = scrapy.Field()
+    chapter_title = scrapy.Field()
+    chapter_group = scrapy.Field()
+    chapter_url = scrapy.Field()
+    chapter_vip = scrapy.Field()
+    chapter_word_count = scrapy.Field()
+    chapter_updated = scrapy.Field()
---
+++ psNovel/spiders/jjwxc.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+from psNovel.items import Novel, Chapter
+from bs4 import *
+from urllib import parse
+
+
+class JjwxcSpider(scrapy.Spider):
+    name = 'jjwxc'
+    allowed_domains = ['jjwxc.net']
+
+    def start_requests(self):
+        urls = [
+            # 'http://www.jjwxc.net/onebook.php?novelid=1888526',
+            'http://www.jjwxc.net/onebook.php?novelid=3160517'
+            # 'http://www.jjwxc.net/onebook.php?novelid=65066'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        soup = BeautifulSoup(response.body, "lxml")
+
+        novel = Novel()
+        novel['title'] = response.xpath('//span[@class="bigtext"]//span/text()').extract_first()
+        novel['author'] = response.xpath('//span[@itemprop="author"]/text()').extract_first()
+
+        novel['intro'] = soup.find('div', {"id": "novelintro"}).text
+
+        novel_metadata = response.xpath('//ul[@name="printright"]/li').extract()
+        # 第一行：文章类型
+        novel['genre'] = Selector(text=novel_metadata[0]).xpath('//span[@itemprop="genre"]/text()').extract_first()\
+            .strip()
+        # 第二行：作品视角
+        novel['view'] = Selector(text=novel_metadata[1]).xpath('//li/text()').extract_first().strip()
+
+        # 第三行：作品风格
+        novel['style'] = Selector(text=novel_metadata[2]).xpath('//li/text()').extract_first().strip()
+
+        # 第四行：作品系列
+        novel['series'] = Selector(text=novel_metadata[3]).xpath('//span[@itemprop="series"]/text()').extract_first()\
+            .strip()
+
+        # 第五行：文章进度
+        if (Selector(text=novel_metadata[4]).xpath('//span[@itemprop="updataStatus"]/font')):
+            novel['updateStatus'] = Selector(text=novel_metadata[4]).xpath(
+                '//span[@itemprop="updataStatus"]/font/text()') \
+                .extract_first().strip()
+        else:
+            novel['updateStatus'] = Selector(text=novel_metadata[4]).xpath('//span[@itemprop="updataStatus"]/text()')\
+                .extract_first().strip()
+
+        # 第六行：全文字数
+        novel['wordCount'] = Selector(text=novel_metadata[5]).xpath('//span[@itemprop="wordCount"]/text()')\
+            .extract_first().strip()
+
+        # 第七行：是否出版
+        published = BeautifulSoup(novel_metadata[6], "lxml").text
+        novel['published'] = published.replace("是否出版：", "").replace("（联系出版）", "").strip()
+
+        # 第八行：签约状态
+        novel['signed'] = Selector(text=novel_metadata[7]).xpath('//font/text()').extract_first().strip()
+
+        comment = Selector(text=novel_metadata[8]).xpath('//div[@id="marknovel_message"]/text()')
+        if comment:
+            novel['comment'] = comment.extract_first()
+        else:
+            novel['comment'] = ''
+
+        poster = parse.urlsplit(soup.find('img', {"itemprop": "image"})["src"]).path.split('/')[-1]
+
+        novel["poster"] = poster
+        tags = []
+        html_tags = soup.findAll("div", {"class": "smallreadbody"})[-1].findAll("font")
+        for html_tag in html_tags:
+            tags.append(html_tag.text.strip())
+        novel["tags"] = '|'.join(tags)
+
+        html_keys = soup.findAll("div", {"class": "smallreadbody"})[-1].find("span", {"class": "bluetext"}).text
+        key_array = html_keys.replace("搜索关键字：", "").split("┃")
+        key_leadings = key_array[0].replace("主角：", "").strip().split("，")
+        key_supportings = key_array[1].replace("配角：", "").strip().split("，")
+        key_other = key_array[2].replace("其它：", "").strip().split("，")
+
+        novel["key_leadings"] = key_leadings
+        novel["key_supportings"] = key_supportings
+        novel["key_other"] = key_other
+
+        soup_table = soup.find("table", {"id": "oneboolt"})
+
+        soup_lines = soup_table.findAll('tr')[3:-1]
+        current_group = ""
+        for soup_line in soup_lines:
+            soup_tds = soup_line.findAll("td")
+            if len(soup_tds) == 1:
+                current_group = soup_tds[0].text
+            else:
+                chapter = Chapter()
+                if soup_tds[1].find('a').has_attr("href"):
+                    chapter['chapter_vip'] = "No"
+                    chapter['chapter_url'] = soup_tds[1].find('a')['href']
+                else:
+                    chapter['chapter_vip'] = "Yes"
+                    chapter['chapter_url'] = soup_tds[1].find('a')['rel']
+                chapter['novel_id'] = 0
+                chapter['chapter_group'] = current_group
+                chapter['chapter_id'] = soup_tds[0].text.strip()
+                chapter['chapter_title'] = soup_tds[1].text.strip()
+                chapter['chapter_summary'] = soup_tds[2].text.strip()
+                chapter['chapter_word_count'] = soup_tds[3].text.strip()
+                if(len(soup_tds) == 5):
+                    chapter['chapter_updated'] = soup_tds[4].text.strip()
+                else:
+                    chapter['chapter_updated'] = soup_tds[5].text.strip()
+                if (chapter['chapter_vip'] == "No"):
+                    yield scrapy.Request(url=chapter['chapter_url'], callback=self.parseChapter)
+        yield(novel)
+
+    def parseChapter(self, response):
+        soup = BeautifulSoup(response.body, "lxml")
+        soup_text = soup.find("div", {"class": "noveltext"})
+        soup_divs = soup.findAll("div")
+        for soup_div in soup_divs:
+            soup_div.extract()
+        soup_hrs = soup.findAll("hr")
+        for soup_hr in soup_hrs:
+            soup_hr.extract()
+        str_content = str(soup_text).replace("<br/>", "\r\n").replace('<hr size="1"/>', "")
+        soup_content = BeautifulSoup(str_content, "lxml").text
+        chapter_lines = []
+        lines = (str(soup_content).split("\r\n"))
+        for line in lines:
+            chapter_lines.append(line.strip())
+        chapter_content = "\r\n".join(chapter_lines)
+        print(chapter_content)
